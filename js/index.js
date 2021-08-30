cb2ed273f861e7d542c239005610f4f4ebbbb4f3
jyo-linux <jyokari51@gmail.com>
2021-08-29 20:10:00 -0500

    Conexion de Vue con HTML -Bill, -Selected tip porcent y porcent custom

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const app = new Vue ({
+    el: '#app',
+    data: {
+        bill: '',
+        radio: '',
+        pcustom: '',
+        numPerson: '',
+        tipAmoun: '',
+        totPerson: ''
+    },
+    methods: {}
+})
