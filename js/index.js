bee1c12cc647cd554cb7c4c826bf730dff30e737
jyo-linux <jyokari51@gmail.com>
2021-08-29 22:13:33 -0500

    Calculos y reaccion de tipAmoun y totalPerson

--- js/index.js
+++ js/index.js
@@ -5,17 +5,59 @@ const app = new Vue ({
         radio: '',
         pcustom: '',
         numPerson: '',
-        tipAmoun: '',
-        totPerson: ''
+        tipAmoun: ''
     },
-    methods: {},
+    methods: {
+        reset() {
+            this.bill = '';
+            this.radio = '';
+            this.pcustom = '';
+            this.numPerson = '';
+            this.tipAmoun = '';
+        }
+    },
     computed: {
-        porcent() {
+        computedPorcent() {
             if (this.pcustom != '') {
                 this.radio = '';
                 return this.pcustom;
             } else {
                 return this.radio;
+            }
+        },
+        computedTipAmoun() {
+            if (Number(this.numPerson) > 0 && Number(this.bill) > 0) {
+                if (this.radio != '') {
+                    switch (this.radio) {
+                        case '5%':
+                            this.tipAmoun = (this.bill * .05) / this.numPerson;
+                            break;
+                        case '10%':
+                            this.tipAmoun = (this.bill * .1) / this.numPerson;
+                            break;
+                        case '15%':
+                            this.tipAmoun = (this.bill * .15) / this.numPerson;
+                            break;
+                        case '25%':
+                            this.tipAmoun = (this.bill * .25) / this.numPerson;
+                            break;
+                        default:
+                            this.tipAmoun = (this.bill * .5) / this.numPerson;
+                            break;
+                    }
+                } else if(this.pcustom != '') {
+                    this.tipAmoun = (this.bill * (Number(this.pcustom) / 100)) / this.numPerson;
+                }
+                return this.tipAmoun.toFixed(2);
+            } else {
+                return '0.00';
+            }
+        },
+        computedTotPerson() {
+            if (this.tipAmoun != '') {
+                return ((this.bill / this.numPerson) + this.tipAmoun).toFixed(2);
+            } else {
+                return '0.00';
             }
         }
     }
